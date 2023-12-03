3a1246ba387e374582487506afc03d66571e255a
Ye Lin Aung <me@yelinaung.com>
2023-12-03 18:40:40 +0800

    redo day1 part1

--- day_1/solution_part_1.py
+++ day_1/solution_part_1.py
@@ -1,12 +1,13 @@
-with open("input.txt") as file:
-    lines = file.readlines()
+def num(s: str) -> int:
+    x = [c for c in s if c.isdigit()]
+    return int(f"{x[0]}{x[-1]}")
 
-digits = []
-for line in lines:
-    digits.append([i for i in list(line.strip()) if i.isdigit()])
 
-total = 0
-for digit in digits:
-    total += int(f"{digit[0]}{digit[-1]}")
+lines = open("input.txt").readlines()
+total = sum([num(line) for line in lines])
+
+# total = 0
+# for digit in digits:
+#     total += int(f"{digit[0]}{digit[-1]}")
 
 print(total)
